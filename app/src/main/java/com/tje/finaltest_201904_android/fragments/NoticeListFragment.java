c8e61c28e6fe2b7c37c2d7ceaa127ef0c89fff6d
Kyung Jin Cho <kj_cho@idealidea.co.kr>
2019-05-19 15:12:10 +0900

    공지사항 프래그먼트 생성

---
+++ app/src/main/java/com/tje/finaltest_201904_android/fragments/NoticeListFragment.java
@@ -0,0 +1,34 @@
+package com.tje.finaltest_201904_android.fragments;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tje.finaltest_201904_android.R;
+import com.tje.finaltest_201904_android.databinding.FragmentMyProfileBinding;
+import com.tje.finaltest_201904_android.databinding.FragmentNoticeListBinding;
+
+public class NoticeListFragment extends Fragment {
+
+    FragmentNoticeListBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_notice_list, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+    }
+
+}
