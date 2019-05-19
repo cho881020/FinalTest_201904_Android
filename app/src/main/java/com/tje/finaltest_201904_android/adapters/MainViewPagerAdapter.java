95c44605639a08f3a08662e2079c7d1ab43d96b5
Kyung Jin Cho <kj_cho@idealidea.co.kr>
2019-05-19 15:16:59 +0900

    뷰페이저 어댑터까지 생성

---
+++ app/src/main/java/com/tje/finaltest_201904_android/adapters/MainViewPagerAdapter.java
@@ -0,0 +1,49 @@
+package com.tje.finaltest_201904_android.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.tje.finaltest_201904_android.fragments.MyProfileFragment;
+import com.tje.finaltest_201904_android.fragments.NoticeListFragment;
+
+public class MainViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    MyProfileFragment frag1 = null;
+    NoticeListFragment frag2 = null;
+
+    int mNumOfTabs;
+
+    public MainViewPagerAdapter(FragmentManager fm, int numOfTabs) {
+        super(fm);
+        mNumOfTabs = numOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        Fragment fr = null;
+
+        if (position == 0) {
+            if (frag1 == null) {
+                frag1 = new MyProfileFragment();
+            }
+            fr = frag1;
+        }
+        else if (position == 1) {
+            if (frag2 == null) {
+                frag2 = new NoticeListFragment();
+            }
+
+            fr = frag2;
+        }
+
+        return fr;
+
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
